a50b838b3ef7df98042877d8bf3e3842b1965636
leejuhyeok <tyrfingr12@naver.com>
2019-02-26 18:14:00 +0900

    124숫자의 나라

---
+++ src/main/java/숫자124의나라/Solution.java
@@ -0,0 +1,18 @@
+package 숫자124의나라;
+
+public class Solution {
+    public String solution(int n) {
+        String answer = "";
+        int[] arr = {4, 1, 2};
+        int a;
+        while (n > 0) {
+            a = n % 3;
+            n = n / 3;
+            if (a == 0) {
+                n -= 1;
+            }
+            answer = arr[a] + answer;
+        }
+        return answer;
+    }
+}
